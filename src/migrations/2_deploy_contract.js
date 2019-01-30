e85190382936c781f7569cadcde20554c751dc4c
Diego Baranowski <diego.baranowski@enerminds.com>
2019-01-30 15:43:08 -0300

    corrected migrations

--- src/migrations/2_deploy_contract.js
+++ src/migrations/2_deploy_contract.js
@@ -1,13 +1,14 @@
-var sucToken = artifacts.require("sucToken");
+const sucToken = artifacts.require("../contracts/principal/sucToken.sol");
 
 
 //import "./nfTokenMetadata.sol";
 //import "./nfTokenEnumerable.sol";
 //import "./ownable.sol";
 
-module.exports = function (deployer, network, accounts) {
+module.exports = function (deployer) {
   //deployer.deploy(Destructible).then(function () {
     //deployer.link(Destructible, sucToken)
-    return deployer.deploy(sucToken)
+    //return deployer.deploy(sucToken)
+    deployer.deploy(sucToken, "sucToken1", "suc");
   //});
 };
