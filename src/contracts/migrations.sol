5428a27e80f85084df03cac964c7ea4f7ece5bc5
Diego Baranowski <diego.baranowski@enerminds.com>
2019-02-04 00:21:23 -0300

    corrected CI Error: Variable name must be in mixedCase

--- src/contracts/migrations.sol
+++ src/contracts/migrations.sol
@@ -2,7 +2,7 @@ pragma solidity 0.5.2;
 
 contract Migrations {
   address public owner;
-  uint public last_completed_migration;
+  uint public lastCompletedMigration;
 
   modifier restricted() {
     if (msg.sender == owner) _;
@@ -13,11 +13,11 @@ contract Migrations {
   }
 
   function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
+    lastCompletedMigration = completed;
   }
 
   function upgrade(address newAddress) public restricted {
     Migrations upgraded = Migrations(newAddress);
-    upgraded.setCompleted(last_completed_migration);
+    upgraded.setCompleted(lastCompletedMigration);
   }
 }
