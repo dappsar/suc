d18ec215655fc0cf3c0dda16096a39136fa68e0e
Diego Baranowski <diego.baranowski@enerminds.com>
2019-03-04 13:23:51 -0300

    a few changes

--- src/migrations/2_deploy_contract.js
+++ src/migrations/2_deploy_contract.js
@@ -1,10 +1,14 @@
 const sucToken = artifacts.require("./sucToken.sol");
+const proxy = artifacts.require("./proxy.sol");
 
 module.exports = function (deployer, network) {
     if (network.startsWith("alastria") || network.startsWith("telsius")) {
       web3.personal.unlockAccount("0x42339e31a153db90c4f9af3326fc9c541b18225e", "l@z@r0");
     }
-    deployer.deploy(sucToken, "sucToken1", "suc").then(function(){
-      return console.log(sucToken.address);
+
+    deployer.deploy(proxy).then(function(){
+      deployer.deploy(sucToken, "sucToken1", "suc");
+      console.log("Proxy Address: " + proxy.address);
+      return console.log("SucToken Address: " + sucToken.address);
     });
 };
