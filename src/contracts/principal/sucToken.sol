e6a5253fd3a17147ca1fb2cb04bbab9a175d8611
Diego Baranowski <diego.baranowski@enerminds.com>
2019-03-17 17:51:50 -0300

    corrected solhint

--- src/contracts/principal/sucToken.sol
+++ src/contracts/principal/sucToken.sol
@@ -13,14 +13,14 @@ contract SucToken is
   Ownable
 {
 
-	// Usado para detener el contrato (parte de implementación de circuit-breakers)
-	bool private stopped = false;
+  // Usado para detener el contrato (parte de implementación de circuit-breakers)
+  bool private stopped = false;
 
   /**
-   * @dev Contract constructor.
-   * @param _name A descriptive name for a collection of NFTs.
-   * @param _symbol An abbreviated name for NFTokens.
-   */
+    * @dev Contract constructor.
+    * @param _name A descriptive name for a collection of NFTs.
+    * @param _symbol An abbreviated name for NFTokens.
+    */
   constructor(
     string memory _name,
     string memory _symbol
@@ -32,11 +32,11 @@ contract SucToken is
   }
 
   /**
-   * @dev Mints a new NFT.
-   * @param _to The address that will own the minted NFT.
-   * @param _tokenId of the NFT to be minted by the msg.sender.
-   * @dev 'stopInEmergency' circuit-breaker pattern implementation
-   */
+    * @dev Mints a new NFT.
+    * @param _to The address that will own the minted NFT.
+    * @param _tokenId of the NFT to be minted by the msg.sender.
+    * @dev 'stopInEmergency' circuit-breaker pattern implementation
+    */
   function mint(
     address _to,
     uint256 _tokenId
@@ -49,11 +49,11 @@ contract SucToken is
   }
 
   /**
-   * @dev Mints a new NFT.
-   * @param _to The address that will own the minted NFT.
-   * @param _tokenId of the NFT to be minted by the msg.sender.
-   * @param _uri String representing RFC 3986 URI.
-   */
+    * @dev Mints a new NFT.
+    * @param _to The address that will own the minted NFT.
+    * @param _tokenId of the NFT to be minted by the msg.sender.
+    * @param _uri String representing RFC 3986 URI.
+    */
   function mint(
     address _to,
     uint256 _tokenId,
@@ -68,36 +68,36 @@ contract SucToken is
   }
 
   /**
-   * @dev Removes a NFT from owner.
-   * @param _tokenId Which NFT we want to remove.
-   * @dev 'stopInEmergency' circuit-breaker pattern implementation
-   */
+    * @dev Removes a NFT from owner.
+    * @param _tokenId Which NFT we want to remove.
+    * @dev 'stopInEmergency' circuit-breaker pattern implementation
+    */
   function burn(uint256 _tokenId) external onlyOwner stopInEmergency
   {
     super._burn(_tokenId);
   }
 
-	/**
-		* @dev Cambiar un flag (stopped) en caso de algun error que requiera la 
-		* @dev implementación de detención de código (circuit-breaker)
-		* @dev Hace uso del modificador 'onlyOwner' de la interfaz @Ownable
-		*/
-	function toggleContractActive() onlyOwner external
-	{
-		stopped = !stopped;
-	}
+  /**
+    * @dev Cambiar un flag (stopped) en caso de algun error que requiera la 
+    * @dev implementación de detención de código (circuit-breaker)
+    * @dev Hace uso del modificador 'onlyOwner' de la interfaz @Ownable
+    */
+  function toggleContractActive() external onlyOwner
+  {
+    stopped = !stopped;
+  }
 
-	/**
-		* @dev Implementación de modificador para detener la ejecución de una función
-		* @dev en caso de emergencia
-	  */
-	modifier stopInEmergency { if (!stopped) _; }
+  /**
+    * @dev Implementación de modificador para detener la ejecución de una función
+    * @dev en caso de emergencia
+    */
+  modifier stopInEmergency { if (!stopped) _; }
 
-	/**
-		* @dev Implementación de modificador para ejecución de una función solo en caso
-		* @dev de emergencia
-		* @dev por el momento, modificador no utiliza den alguna función
-		*/
-	modifier onlyInEmergency { if (stopped) _; }
+  /**
+    * @dev Implementación de modificador para ejecución de una función solo en caso
+    * @dev de emergencia
+    * @dev por el momento, modificador no utiliza den alguna función
+    */
+  modifier onlyInEmergency { if (stopped) _; }
 
 }
